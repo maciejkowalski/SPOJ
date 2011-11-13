52e93bf40d86c85e77b1b498dc5762d4f86dc378
Maciej 'Hitsu' Kowalski <mkoval33@gmail.com>
2011-11-13 15:49:30 +0100

    Update Scala/rno_dod.scala

--- Scala/rno_dod.scala
+++ Scala/rno_dod.scala
@@ -15,27 +15,3 @@ object Main {
       array.foreach(println)
   }
 }
-
-
-/*
-test = gets
-test = Integer(test)
-#
-ile=[]
-n=[]
-test.times  { |it|
-  i = Integer(it)
-  ile[i] = gets
-  ile[i] = Integer(ile[i])
-  n[it] = gets.split(" ")
-}
-
-wynik=[]
-test.times { |e|
-  wynik[e] = 0
-  ile[e].times { |i|
-    wynik[e] += Integer(n[e][i])
-  }
-  puts wynik[e]
-}
-*/
